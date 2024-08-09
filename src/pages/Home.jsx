69239df09ef56736d732190bbdcdc62cd6a057c5
arnelsinadhan19 <arnelsinadhan19@gmail.com>
2024-08-09 09:48:37 +0800

    modified home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import { Container } from "react-bootstrap";
 import GetPost from "../components/GetPost";
-
+import { UserContext } from "../UserContext";
+import { useContext } from "react";
+import Login from "../pages/Login";
 export default function Home() {
+  const { isAuthenticated } = useContext(UserContext);
+
   return (
     <Container fluid className="home">
-      <GetPost />
+      {!isAuthenticated ? <GetPost /> : <Login />}
     </Container>
   );
 }
